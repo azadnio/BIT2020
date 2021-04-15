8fd2a9494e16d475d93f76d10cbd8718fdc6b39b
azad <azad@siliconlogics.com>
2021-04-15 17:02:37 +0530

    changes

---
+++ PPL/js/app.js
@@ -0,0 +1,6 @@
+$(function(){
+
+
+    console.log('loading');
+    $("#tabs").tabs();
+});
---
+++ PPL/js/global.js
@@ -0,0 +1,31 @@
+const TEAMS_NAMES = [
+    'THENNA LIONS',
+    'MALGAMANDENIYA ',
+    'HEEPITIYA',
+    'ALIYATENNA',
+    'THAAPPA',
+    'HENTHENNA',
+    'J M KINGS',
+    'PANGOLLAMADA'
+];
+
+
+class Team {
+
+    constructor(name, captain, sponsor){
+        this.name = name;
+        this.captain = captain;
+        this.sponsor = sponsor;
+    }
+}
+
+class Player {
+
+    constructor(name, role, team, img, basePrice = 1000) {
+        this.name = name;
+        this.role = role;
+        this.team = team;
+        this.img = img;
+        this.basePrice = basePrice;
+    }
+}
